9e457bcf9b50dcb3b1c3b59f987743cbc31e3f80
Alyssa Vivien <alyssavivien.sc@gmail.com>
2022-12-16 12:39:33 +0800

    Add exception handling to FoodOrdering

--- src/FoodOrdering/FoodOrderGUI.java
+++ src/FoodOrdering/FoodOrderGUI.java
@@ -1,7 +1,5 @@
 package FoodOrdering;
 
-import LeapYear.LeapYearGUI;
-
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -46,40 +44,77 @@ public class FoodOrderGUI extends JFrame {
     }
 
     public void result() {
-        double price = 0.0;
+        double price = 0.00;
+        boolean flag = false;
+        int order = 0;
 
         if(cPizza.isSelected()) {
             price += 100;
+            order++;
         }
 
         if(cBurger.isSelected()) {
             price += 80;
+            order++;
         }
 
         if(cFries.isSelected()) {
             price += 65;
+            order++;
         }
 
         if(cSoftDrinks.isSelected()) {
             price += 55;
+            order++;
         }
 
         if(cTea.isSelected()) {
             price += 50;
+            order++;
         }
 
         if(cSundae.isSelected()) {
             price += 40;
+            order++;
         }
 
         if(rb5.isSelected()) {
             price = price - (price * 0.05);
+            JOptionPane.showMessageDialog(null,"Total Price is Php " + price);
         } else if(rb10.isSelected()) {
             price = price - (price * 0.10);
+            JOptionPane.showMessageDialog(null,"Total Price is Php " + price);
         } else if(rb15.isSelected()) {
             price = price - (price * 0.15);
+            JOptionPane.showMessageDialog(null,"Total Price is Php " + price);
+        } else if(rbNone.isSelected()) {
+            JOptionPane.showMessageDialog(null,"Total Price is Php " + price);
+        } else {
+            flag = true;
         }
 
-        JOptionPane.showMessageDialog(null,"Total Price is Php " + price);
+        try {
+            if(flag) {
+                throw(new discount("Select one discount."));
+            } else if(order == 0) {
+                throw(new orderr("Select atleast one food."));
+            }
+        } catch(discount a) {
+            JOptionPane.showMessageDialog(null, "Select one discount.");
+        } catch (orderr b) {
+            JOptionPane.showMessageDialog(null, "Select atleast one food.");
+        }
+    }
+
+    class discount extends Exception {
+        public discount (String s) {
+            super(s);
+        }
+    }
+
+    class orderr extends Exception {
+        public orderr (String s) {
+            super(s);
+        }
     }
 }
